8388a07904461ffdae8c99b41d825f5127a9018d
dank <dan.kiernan@bjss.com>
2022-12-08 20:23:13 +0000

    solved day6 part 1

---
+++ day6/part1.py
@@ -0,0 +1,13 @@
+def get_start_marker(line):
+    for index, _ in enumerate(line):
+        packet = [letter for letter in line[index : index + 4]]
+        unique_packet_characters = list(set(packet))
+        if len(packet) == len(unique_packet_characters):
+            return f"{packet}, {index + 4}"
+            
+
+if __name__ == "__main__":
+    with open("day6/input.txt", "r") as file:
+        line = file.readline()
+        print(get_start_marker(line))
+        
