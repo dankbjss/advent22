c58c74bbff0b672ced695877b2de498b6a3677d0
dank <dan.kiernan@bjss.com>
2022-12-08 20:27:54 +0000

    solved day 6 part 2

--- day6/part2.py
+++ day6/part2.py
@@ -1,13 +1,11 @@
-def get_packet_start_marker(line):
+def get_message_start_marker(line):
     for index, _ in enumerate(line):
-        packet = [letter for letter in line[index : index + 4]]
+        packet = [letter for letter in line[index : index + 14]]
         unique_packet_characters = list(set(packet))
         if len(packet) == len(unique_packet_characters):
-            return f"{packet}, {index + 4}"
-            
+            return f"{packet}, {index + 14}" 
 
 if __name__ == "__main__":
     with open("day6/input.txt", "r") as file:
         line = file.readline()
-        print(get_packet_start_marker(line))
-        
+    print(get_message_start_marker(line))
